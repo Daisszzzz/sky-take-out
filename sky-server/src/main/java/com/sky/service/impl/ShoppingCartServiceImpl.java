a4b10d0f57147c3ba7cb0c93734990c51ca11b28
dsz <dai.sszzzz@gmail.com>
2025-04-20 13:25:50 +1000

    查看购物车业务功能开发

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -74,4 +74,16 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
             shoppingCartMapper.insert(shoppingCart);
         }
     }
+
+
+    @Override
+    public List<ShoppingCart> showShoppingCart() {
+        // 获取到当前微信用户的ID
+        Long userID = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = ShoppingCart.builder()
+                .userId(userID)
+                .build();
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+        return list;
+    }
 }
